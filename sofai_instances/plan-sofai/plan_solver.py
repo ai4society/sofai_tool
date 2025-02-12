f6538c9d3f2ec0aaa970dee99d33d6279abeaa2d
FrancescoFabiano <francesco.fabi94@gmail.com>
2025-02-12 17:30:28 +0000

    Added submodule for FD

--- sofai_instances/plan-sofai/plan_solver.py
+++ sofai_instances/plan-sofai/plan_solver.py
@@ -120,7 +120,7 @@ def plan_solve(problem_id):
     thresholds_file="thresholds.txt"
     experience_file = "plan_experience.json"
     new_run = False
-    run_type = "sofai"#"s2"#"sofai"
+    run_type = "sofai" # Possible values: "s1" "s2" "sofai"
     
     meta.metacognition(problem_id, system1_solver, system2_solver, context_file, thresholds_file, experience_file, new_run, run_type)
     
